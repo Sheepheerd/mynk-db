772ec3ad8eae2a7d3a210d5c07c7e712170ba985
Jacob Poling <polingj999@gmail.com>
2025-07-13 22:11:27 +0000

    Moved routed funcs

--- src/example_routes.c
+++ src/example_routes.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include "router.h"
 
-void hello_from_example_routes() {
-    printf("Hello From example routes\n");
+void hello_from_router() {
+    printf("Hello From da routa\n");
+}
+
+void another_route() {
+    printf("Hello From another_route\n");
 }
--- src/router.c
+++ src/router.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include "router.h"
 
-void hello_from_router() {
-    printf("Hello From da routa\n");
-}
-
-void another_route() {
-    printf("Hello From another_route\n");
-}
-
 
 // Route r1 = {&hello_from_router};
 // Route r2 = {&another_route};
